a0a764e360117f9b4c5c595e19c9d554209dafca
Pilar Buchen <pilarbuchen@gmail.com>
2021-05-23 17:38:34 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 // session express session
-const session = require('express-session');
-
-// to make style sheet available to the client
 const path = require('path');
-
-const routes = require('./controllers');
 const express = require("express");
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+// to make style sheet available to the client
 
 const app = express();
 const PORT = process.env.PORT || 3005;
+
+
 
 const sequelize = require("./config/connection");
 // session
@@ -18,7 +17,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
   // Set cookie to expire in 5 minutes
-  cookie: { maxAge: 5 * 60 * 1000 },
+  cookie: { },
   resave: false,
   rolling: true,
   saveUninitialized: true,
@@ -38,13 +37,11 @@ app.set('view engine', 'handlebars');
 
 // to add style sheet and serve as asset
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
 
+app.use(require('./controllers/'));
 // turn on connection to db and server //tables will drop if force:true
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
